a79b9361e4d7114a849f5db0e997e096de570d40
gilmarsan <gilmarsan@gmail.com>
2020-07-11 18:22:03 -0300

    atualizando arquivos

--- synaptrons_ecommerce/views.py
+++ synaptrons_ecommerce/views.py
@@ -2,21 +2,21 @@ from django.http import HttpResponse
 from django .shortcuts import render
 def home_page(request):
     context = {
-        "title": "Página principal"
+        "title": "Página principal",
         "content": "Bem-vindo à Página Principal"
     }
     return render(request, "home_page.html", context)
 
 def about_page(request):
     context = {
-        "title": "Página sobre"
+        "title": "Página sobre",
         "content": "Bem-vindo à página sobre"
     }
-    return render(request, "about_page.html", context)
+    return render(request, "about/view.html", context)
 
 def contact_page(request):
     context = {
-        "title" = "Página de contato"
-        "content" = "Bem-vindo à página de contato"
+        "title": "Página de contato",
+        "content": "Bem-vindo à página de contato"
     }
-    return render(request, "contact_page.html", context)
+    return render(request, "about/view.html", context)
