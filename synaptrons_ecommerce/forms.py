e5511e2c73cf6d8166fd5eaa138057aeb801c727
gilmarsan <gilmarsan@gmail.com>
2020-07-13 10:47:13 -0300

    criando o formulario de login

--- synaptrons_ecommerce/forms.py
+++ synaptrons_ecommerce/forms.py
@@ -1,5 +1,7 @@
 from django import forms
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 class ContactForm(forms.Form):
     nome_completo = forms.CharField(
     error_messages={'required': 'Obrigatório o preenchimento do nome!'},
--- synaptrons_ecommerce/views.py
+++ synaptrons_ecommerce/views.py
@@ -1,6 +1,7 @@
+from django.contrib.auth import authenticate, login, get_user_model
 from django.http import HttpResponse
-from django .shortcuts import render
-from .forms import ContactForm
+from django .shortcuts import render, redirect
+from .forms import ContactForm, LoginForm, RegisterForm
 
 def home_page(request):
     context = {
