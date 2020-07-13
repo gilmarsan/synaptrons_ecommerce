330e4764ce9847a39d744933e3ebeff0f2d2af2f
gilmarsan <gilmarsan@gmail.com>
2020-07-13 18:13:34 -0300

    inserçao da app products

--- products/models.py
+++ products/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model): #product_category
+    title       = models.CharField()= models.CharField(ma_length=120)
+    description = models.TextField()
+    price       = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
