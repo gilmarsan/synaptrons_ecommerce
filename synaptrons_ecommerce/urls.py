5c837c023bc4377f3db10a126faee5efcaa4b322
gilmarsan <gilmarsan@gmail.com>
2020-07-14 10:49:03 -0300

    incluindo > no path de products-fbv/int:pk>

--- synaptrons_ecommerce/urls.py
+++ synaptrons_ecommerce/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
     path('products/', ProductListView.as_view()),
     path('products-fbv/', product_list_view),
     path('products/<int:pk>', ProductDetailView.as_view()),
-    path('products-fbv/<int:pk', product_detail_view),
+    path('products-fbv/<int:pk>', product_detail_view),
     path('admin/', admin.site.urls),
 ]
 
