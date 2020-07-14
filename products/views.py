b8ca896c03187ede25b0116ce5bdfc38c4a405e6
gilmarsan <gilmarsan@gmail.com>
2020-07-14 09:51:00 -0300

    Incluir o método get_context_data em views.py products

--- products/views.py
+++ products/views.py
@@ -9,6 +9,11 @@ class ProductListView(ListView):
     queryset = Product.objects.all()
     template_name = "products/list.html"
 
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductListView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
+
 
 #Function Based View
 def product_list_view(request):
