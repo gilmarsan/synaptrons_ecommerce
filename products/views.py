edac7dc48b2f2a2e49d65e1ad7c808eda8c679dc
gilmarsan <gilmarsan@gmail.com>
2020-07-16 09:50:14 -0300

    Atualizando product_detail_view em views.py de products

--- products/views.py
+++ products/views.py
@@ -25,23 +25,23 @@ def product_list_view(request):
 
 #Class Based View
 class ProductDetailView(DetailView):
-    #traz todos os produtos do banco de dados sem filtrar nada
-    queryset = Product.objects.all()
     template_name = "products/detail.html"
 
     def get_context_data(self, *args, **kwargs):
         context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
         return context
 
+    def get_object(self, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        instance = Product.objects.get_by_id(pk)
+        if instance is None:
+            raise Http404("Esse produto não existe!")
+        return instance
+
 #Function Based View
 def product_detail_view(request, pk = None, *args, **kwargs):
-    #instance = Product.objects.get(pk = pk) #get the object id
-    #instance = get_object_or_404(Product, pk = pk)
-
-    qs = Product.objects.filter(id = pk)
-    if qs.count() == 1:
-        instance = qs.first()
-    else:
+    instance = Product.objects.get_by_id(pk)
+    if instance is None:
         raise Http404("Esse produto não existe!")
 
     context = {
