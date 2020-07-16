b9059f9af68ef5e8ce6d24b138e989b86539bc1a
gilmarsan <gilmarsan@gmail.com>
2020-07-16 16:25:26 -0300

    Feature detail

--- products/views.py
+++ products/views.py
@@ -1,19 +1,26 @@
 from django.http import Http404
+
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
 
 from .models import Product
+
+class ProductFeaturedListView(ListView):
+    template_name = "products/list.html"
+
+    def get_queryset(self, *args, **kwargs):
+        #request = self.request
+        return Product.objects.featured()
+
+class ProductFeaturedDetailView(DetailView):
+    queryset = Product.objects.all().featured()
+    template_name = "products/featured-detail.html"
 
 #Class Based View
 class ProductListView(ListView):
     # traz todos os produtos do banco de dados sem filtrar nada
     queryset = Product.objects.all()
     template_name = "products/list.html"
-
-    #def get_context_data(self, *args, **kwargs):
-    #    context = super(ProductListView, self).get_context_data(*args, **kwargs)
-    #    return context
-
 
 #Function Based View
 def product_list_view(request):
