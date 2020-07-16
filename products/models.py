25c5f335b29d614827be24ba1ea3633beaf15af5
gilmarsan <gilmarsan@gmail.com>
2020-07-16 09:57:04 -0300

    Só corrigir sintaxe

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class ProductManager(models.Manager):
     def get_by_id(self, id):
-        qs = self.get_queryser().filter(id = id)
+        qs = self.get_queryset().filter(id = id)
         if qs.count() == 1:
             return qs.first()
         return None
