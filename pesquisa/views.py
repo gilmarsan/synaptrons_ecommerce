55471f8189bf765c7bbf2cd992008c93777596d4
gilmarsan <gilmarsan@gmail.com>
2020-07-20 14:07:34 -0300

    Atualizaçao do arquivo views.py da pesquisa

--- pesquisa/views.py
+++ pesquisa/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-
 from django.views.generic import ListView
 from products.models import Product
 
 class PesquisaProductView(ListView):
-    template_neme = "products/list.html"
-    def get_queryset(self, *args, **kwargs):
+    template_name = "products/list.html"
+    def get_queryset(self, *args, **kargs):
         request = self.request
-        return Product.objects.all()
+        result = request.GET
+        query = result.get('q', None) # result['q']
+        if query is not None:
+            return Product.objects.filter(title__icontains = query)
+        return Product.objects.featured()
