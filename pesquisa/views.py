dd93ddcc0d70e353f5179fbd2bbc85be7851643d
gilmarsan <gilmarsan@gmail.com>
2020-07-20 12:58:35 -0300

    Adicionar o codigo em views.py da app pesquisa

--- pesquisa/views.py
+++ pesquisa/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView
+from products.models import Product
+
+class PesquisaProductView(ListView):
+    template_neme = "products/list.html"
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        return Product.objects.all()
